1de9b58d68bf00c12eb928a28e6a98521647d0df
Aizar Farhan <andiaizarfarhan@gmail.com>
2025-09-04 12:56:05 +0700

    update console log for better information

--- Tests/LoginTest.spec.ts
+++ Tests/LoginTest.spec.ts
@@ -6,11 +6,13 @@ test.describe('Login Functionality Tests', () => {
     const loginPage = new LoginPage(page);
     await loginPage.goto();
     await loginPage.loginwithValidCredentials();
+    console.log("✅ Berhasil login dan diarahkan ke halaman homepage / dashboard");
   });
 
   test('Sebagai Admin, saya tidak dapat login menggunakan data yang tidak valid', async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.goto();
     await loginPage.loginwithInvalidCredentials();
+    console.log("✅ Tidak berhasil login dan tetap berada di login page");
   });
 });
